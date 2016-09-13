4a3d573d755d97e3cc2617814c7eae761be039e5
zjx20 <zhoujianxiong2@gmail.com>
2016-09-13 16:18:02 +0800

    Added "-t" cmd option, to specify the template file for generating stub methods

--- impl.go
+++ impl.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"go/ast"
 	"go/build"
@@ -19,18 +20,61 @@ import (
 	"golang.org/x/tools/imports"
 )
 
-const usage = `impl <recv> <iface>
+const (
+	usage = `Usage: impl [options...] <recv> <iface>
 
-impl generates method stubs for recv to implement iface.
+  impl generates method stubs for recv to implement iface.
 
 Examples:
 
-impl 'f *File' io.Reader
-impl Murmur hash.Hash
+  impl 'f *File' io.Reader
+  impl Murmur hash.Hash
 
-Don't forget the single quotes around the receiver type
-to prevent shell globbing.
+  Don't forget the single quotes around the receiver type
+  to prevent shell globbing.
 `
+
+	defaultTemplate = "func ({{.Recv}}) {{.Name}}" +
+		"({{range .Params}}{{.Name}} {{.Type}}, {{end}})" +
+		"({{range .Res}}{{.Name}} {{.Type}}, {{end}})" +
+		"{\n" + "panic(\"not implemented\")" + "}\n\n"
+)
+
+var (
+	recv     string
+	iface    string
+	tmplFile string
+	tmpl     = template.Must(template.New("stub").Parse(defaultTemplate))
+)
+
+func parseCmd() {
+	flag.CommandLine.Usage = func() {
+		fmt.Fprint(os.Stderr, usage)
+		fmt.Fprintln(os.Stderr, "\nOptions:\n")
+		flag.CommandLine.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "")
+	}
+	flag.StringVar(&tmplFile, "t", "", "`template file` for generating stub methods")
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		flag.CommandLine.Usage()
+		os.Exit(2)
+	}
+
+	recv = flag.Arg(0)
+	iface = flag.Arg(1)
+
+	if tmplFile != "" {
+		var err error
+		tmpl, err = template.ParseFiles(tmplFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to load template file: %s", err.Error())
+			os.Exit(2)
+		}
+	}
+
+}
 
 // findInterface returns the import path and identifier of an interface.
 // For example, given "http.ResponseWriter", findInterface returns
@@ -249,13 +293,6 @@ func funcs(iface string) ([]Func, error) {
 	return fns, nil
 }
 
-const stub = "func ({{.Recv}}) {{.Name}}" +
-	"({{range .Params}}{{.Name}} {{.Type}}, {{end}})" +
-	"({{range .Res}}{{.Name}} {{.Type}}, {{end}})" +
-	"{\n" + "panic(\"not implemented\")" + "}\n\n"
-
-var tmpl = template.Must(template.New("test").Parse(stub))
-
 // genStubs prints nicely formatted method stubs
 // for fns using receiver expression recv.
 // If recv is not a valid receiver expression,
@@ -287,11 +324,7 @@ func validReceiver(recv string) bool {
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprint(os.Stderr, usage)
-		os.Exit(2)
-	}
-	recv, iface := os.Args[1], os.Args[2]
+	parseCmd()
 	if !validReceiver(recv) {
 		fatal(fmt.Sprintf("invalid receiver: %q", recv))
 	}
